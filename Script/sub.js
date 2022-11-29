a205a0906f1f7947db09fce1251d333bd2081493
l13304648 <62975055+l13304648@users.noreply.github.com>
2022-11-29 15:30:13 +0800

    Create sub.js

---
+++ Script/sub.js
@@ -0,0 +1,29 @@
+/*
+通用订阅解锁
+
+[rewrite_local]
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/l13304648/QunX-Script/main/Script/sub.js
+
+[mitm]
+hostname = buy.itunes.apple.com
+
+*/
+
+
+let body= $response.body;
+var obj = JSON.parse(body);
+if (body.indexOf("expires") !=-1) {
+  obj["receipt"]["in_app"][0]["expires_date"] = "2099-09-09 20:12:02 Etc/GMT";
+  obj["receipt"]["in_app"][0]["expires_date_pst"] = "2099-09-09 12:12:02 America/Los_Angeles";
+  obj["receipt"]["in_app"][0]["expires_date_ms"] = "4092647115000";
+  obj["receipt"]["in_app"][0]["is_trial_period"] = "false";
+  obj["receipt"]["in_app"][0]["is_in_intro_offer_period"] = "false";
+  obj["receipt"]["in_app"][0]["in_app_ownership_type"] = "PURCHASED";
+  obj["latest_receipt_info"][0]["expires_date"] = "2099-09-09 20:12:02 Etc/GMT";
+  obj["latest_receipt_info"][0]["expires_date_pst"] = "2099-09-09 12:12:02 America/Los_Angeles";
+  obj["latest_receipt_info"][0]["expires_date_ms"] = "4092647115000";
+  obj["latest_receipt_info"][0]["is_trial_period"] = "false";
+  obj["latest_receipt_info"][0]["is_in_intro_offer_period"] = "false";
+  obj["latest_receipt_info"][0]["in_app_ownership_type"] = "PURCHASED";
+  }
+$done({body: JSON.stringify(obj)});
