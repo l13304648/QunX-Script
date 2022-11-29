aa2939a143f464c3555bbd85d2fb3963af0f9dd5
l13304648 <62975055+l13304648@users.noreply.github.com>
2022-11-29 14:34:01 +0800

    Create cytq.js

---
+++ Script/cytq.js
@@ -0,0 +1,28 @@
+/*
+#彩云天气
+Svip解锁
+
+[rewrite_local]
+^https:\/\/biz\.caiyunapp\.com\/v2\/user\?app\_name\=weather url script-response-body cytq.js
+
+[MITM]
+hostname = biz.caiyunapp.com
+
+*/
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+obj.result.svip_expired_at = 4102415999,
+obj.result.is_primary = true,
+obj.result.free_trial = 365,
+obj.result.vip_type = "s",
+obj.result.wt.free_trial = 365,
+obj.result.wt.vip.svip_apple_expired_at = 4102415999,
+obj.result.wt.vip.svip_expired_at = 4102415999,
+obj.result.svip_take_effect = 1,
+obj.result.is_vip = true,
+
+body = JSON.stringify(obj);
+$done({body});
