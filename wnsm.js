60b12da5aea4ad721118809d70b2e388c8b3f7d9
l13304648 <62975055+l13304648@users.noreply.github.com>
2022-11-29 11:20:26 +0800

    Create wnsm.js
    蜗牛睡眠永久VIP

---
+++ wnsm.js
@@ -0,0 +1,36 @@
+/*
+#蜗牛睡眠
+vip解锁
+
+
+[rewrite_local]
+^https:\/\/snailsleep\.net\/snail\/v1\/profile\/get\? url script-response-body wnsm2.js
+
+[MITM]
+hostname = snailsleep.net
+
+*/
+let obj = JSON.parse($response.body);
+obj={
+  "status": 200,
+  "message": "OK",
+  "result": {
+    "unique": "",
+    "expires": 2800000000000,
+    "expiresRAD": 2800000000000,
+    "dreamsTotal": 10,
+    "dreamsUsed": 0,
+    "updated": 1648977374317,
+    "points": 999,
+    "level": 3,
+    "offerPeriod": true,
+    "type": "SNAIL",
+    "lifelong": true,
+    "expired": false,
+    "radexpired": false,
+    "links": []
+  },
+  "links": []
+}
+
+$done({body: JSON.stringify(obj)});
