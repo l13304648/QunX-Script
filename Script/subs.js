df582e7068e8462ab7ecf44a53903bda212428c2
l13304648 <62975055+l13304648@users.noreply.github.com>
2022-11-29 15:48:35 +0800

    Update subs.js

--- Script/subs.js
+++ Script/subs.js
@@ -8,33 +8,45 @@ hostname = api.revenuecat.com
 
 */
 
-var obj = {"request_date":"2020-02-15T07:09:49Z",
-  "request_date_ms":"1581750589992",
-  "subscriber":{
-    "entitlements":{
-      "premium":{
-        "expires_date":"2099-09-09T07:07:58Z",
-      "product_identifier":"com.neybox.pillow.premium.year",
-      "purchase_date":"2020-02-15T07:07:58Z"}
-      },
-      "first_seen":"2020-02-14T20:28:01Z",
-      "last_seen":"2020-02-14T20:28:01Z",
-      "non_subscriptions":{},
-      "original_app_user_id":"D1D6D98B-EF51-48AF-9876-7352ABCEFD60",
-      "original_application_version":"216",
-      "original_purchase_date":"2020-02-14T20:26:59Z",
-      "other_purchases":{},
-      "subscriptions":{
-        "com.neybox.pillow.premium.year":{
-          "billing_issues_detected_at":null,
-          "expires_date":"2099-09-09T07:07:58Z",
-          "is_sandbox":false,
-"original_purchase_date":"2020-02-15T07:07:58Z",
-          "period_type":"normal",
-          "purchase_date":"2020-02-15T07:07:58Z",
-          "store":"app_store",
-          "unsubscribe_detected_at":null}
-          }
+var obj = {
+  "request_date_ms" : 1665728663210,
+  "request_date" : "2022-10-14T06:24:23Z",
+  "subscriber" : {
+    "non_subscriptions" : {
+
+    },
+    "first_seen" : "2022-10-14T06:20:43Z",
+    "original_application_version" : "251",
+    "other_purchases" : {
+
+    },
+    "management_url" : "https://apps.apple.com/account/subscriptions",
+    "subscriptions" : {
+      "com.lkzhao.editor.full" : {
+        "is_sandbox" : false,
+        "ownership_type" : "PURCHASED",
+        "billing_issues_detected_at" : null,
+        "period_type" : "pro",
+        "expires_date" : "",
+        "grace_period_expires_date" : null,
+        "unsubscribe_detected_at" : null,
+        "original_purchase_date" : "2022-10-14T06:23:55Z",
+        "purchase_date" : "2022-10-14T06:23:53Z",
+        "store" : "app_store"
       }
- }
+    },
+    "entitlements" : {
+      "pro" : {
+        "grace_period_expires_date" : null,
+        "purchase_date" : "2022-10-14T06:23:53Z",
+        "product_identifier" : "com.lkzhao.editor.full",
+        "expires_date" : ""
+      }
+    },
+    "original_purchase_date" : "2022-10-14T06:18:56Z",
+    "original_app_user_id" : "$RCAnonymousID:4024fefebf014458bef29f2e013c5124",
+    "last_seen" : "2022-10-14T06:20:43Z"
+  }
+}
+
 $done({body: JSON.stringify(obj)});
